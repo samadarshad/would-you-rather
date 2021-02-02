9c65b08728589a0c37a0ab7fa9fece30a4f6c464
samad <samadarshad@hotmail.co.uk>
2021-02-02 17:30:41 +0000

    Draft for questionview

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Nav from './components/Nav'
 import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
+import QuestionView from './views/QuestionView'
 
 function App() {
   return (
     <Router>
-
       <>
-        <Nav loggedInUser={'tylermcginnis'} />
+        <Nav />
+        <QuestionView />
       </>
-
     </Router>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+import {
+    Menu,
+    Divider
+} from 'semantic-ui-react'
 
 class Nav extends Component {
-    render() {
+    state = { activeItem: 'home' }
 
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state
 
         return (
-            <nav className='nav'>
-                <ul>
-                    <li>
-                        <NavLink to='/' exact activeClassName='active'>
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/new' activeClassName='active'>
-                            New Tweet
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
+            <div>
+                <Menu pointing secondary>
+                    <Menu.Item
+                        name='home'
+                        active={activeItem === 'home'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='messages'
+                        active={activeItem === 'messages'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='friends'
+                        active={activeItem === 'friends'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Menu position='right'>
+                        <Menu.Item
+                            name='logout'
+                            active={activeItem === 'logout'}
+                            onClick={this.handleItemClick}
+                        />
+                    </Menu.Menu>
+                </Menu>
+                <Divider hidden />
+            </div>
         )
     }
 }
