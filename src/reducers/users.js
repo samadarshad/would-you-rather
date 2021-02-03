bee521d7d163c8134cd770871aaf1cfe72472c17
samad <samadarshad@hotmail.co.uk>
2021-02-03 20:52:50 +0000

    shaping user data input

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,11 +1,23 @@
 import { RECEIVE_USERS } from '../actions/users'
 
+
+
+
 export default function users(state = {}, action) {
     switch (action.type) {
         case RECEIVE_USERS:
+            let shapedUserData = Object.assign(...Object.entries(action.users).map(([_, user]) => (
+                {
+                    [user.id]: {
+                        id: user.id,
+                        name: user.name,
+                        avatarURL: user.avatarURL,
+                    }
+                }
+            )))
             return {
                 ...state,
-                ...action.users
+                ...shapedUserData
             }
         default:
             return state
