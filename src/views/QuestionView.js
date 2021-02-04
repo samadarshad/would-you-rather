c335727c094c2a82c171c12484b41c1cf7308e3d
samad <samadarshad@hotmail.co.uk>
2021-02-04 19:05:05 +0000

    result or question

--- src/views/QuestionView.js
+++ src/views/QuestionView.js
@@ -7,40 +7,71 @@ import {
     Segment,
     Image,
 } from 'semantic-ui-react'
+import { getAnsweredQuestionIdsByUserId } from '../helper'
 import QuestionForm from '../components/QuestionForm'
+import Result from '../components/Result'
+
+const showResult = (user, id) => (
+    <>
+        <Header attached='top' block>
+            Asked by {user.name}
+        </Header>
+        <Segment attached>
+            <Grid columns={3} divided>
+                <Grid.Column className='five wide column'>
+                    <Image src={user.avatarURL} size='medium' circular centered />
+                </Grid.Column>
+                <Grid.Column className='eleven wide column'>
+                    <Result id={id} />
+                </Grid.Column>
+            </Grid>
+        </Segment>
+    </>
+)
+
+const showQuestion = (user, id) => (
+    <>
+        <Header attached='top' block>
+            {user.name} asks:
+        </Header>
+        <Segment attached>
+            <Grid columns={3} divided>
+                <Grid.Column className='five wide column'>
+                    <Image src={user.avatarURL} size='medium' circular centered />
+                </Grid.Column>
+                <Grid.Column className='eleven wide column'>
+                    <QuestionForm id={id} />
+                </Grid.Column>
+            </Grid>
+        </Segment>
+    </>
+)
 
 class QuestionView extends Component {
     //event: submit answer to store, using the currently authed user during the action
     render() {
         return (
             <>
-                <Header attached='top' block>
-                    {this.props.user.name} asks:
-                </Header>
-                <Segment attached>
-                    <Grid columns={3} divided>
-                        <Grid.Column className='five wide column'>
-                            <Image src={this.props.user.avatarURL} size='medium' circular centered verticalAlign='middle' />
-                        </Grid.Column>
-                        <Grid.Column className='eleven wide column'>
-                            <QuestionForm id={this.props.id} />
-                        </Grid.Column>
-                    </Grid>
-                </Segment>
+                {this.props.alreadyAnswered
+                    ? showResult(this.props.user, this.props.id)
+                    : showQuestion(this.props.user, this.props.id)
+                }
             </>
         )
     }
 }
 
-function mapStateToProps({ questions, users }, props) {
+function mapStateToProps({ questions, users, authedUser, answers }, props) {
     const { id } = props.match.params ?? null;
 
     const question = questions[id] ?? null;
     const user = question ? users[questions[id].author] : null;
+    const alreadyAnswered = getAnsweredQuestionIdsByUserId(answers, authedUser).includes(id)
     return {
         id,
         question,
         user,
+        alreadyAnswered
     }
 }
 
