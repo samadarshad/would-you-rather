5e03b44b7caae0ce0917f4055151b7537088ff4d
samad <samadarshad@hotmail.co.uk>
2021-02-05 15:56:24 +0000

    signed out as default

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,13 +2,9 @@ import { _getUsers, _getQuestions } from '../_DATA'
 import { receiveUsers } from '../actions/users'
 import { receiveQuestions } from '../actions/questions'
 import { receiveAnswers } from '../actions/answers'
-import { setAuthedUser } from '../actions/authedUser'
 import { setLoading, LoadingStatus } from '../actions/loading'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
-const AUTHED_ID = 'tylermcginnis'
-
-// move this to api.js file
 function getInitialData() {
     return Promise.all([
         _getUsers(),
@@ -28,7 +24,6 @@ export function handleInitialData() {
                 dispatch(receiveUsers(users))
                 dispatch(receiveQuestions(questions))
                 dispatch(receiveAnswers(questions))
-                dispatch(setAuthedUser(AUTHED_ID))
                 dispatch(setLoading(LoadingStatus.SUCCEEDED))
                 dispatch(hideLoading())
             })
